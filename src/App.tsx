c3195d29109c7a3226b6bd7f1d5980ff9d70e07d
datquang03 <daoquangdat0103@gmail.com>
2024-10-13 21:19:49 +0700

    check loading & loading router fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute";
 
-import AdminLayout from "./defaultLayout/AdminLayout";
 import AdminContent from "./pages/AdminDashboard/AdminContent";
 import UserManagement from "./pages/AdminDashboard/userManagement";
 import RequestUser from "./pages/RequestUser";
-import AdminNavBar from "./components/Admin/AdminNavbar";
-import InstructorNavbar from "./components/Instructor/InstructorNavbar";
-import AdminPage from "./pages/Dashboard/Adminpage";
 import InstructorPage from "./pages/Dashboard/Instructorpage";
 import StudentPage from "./pages/Dashboard/Studentpage";
 import GeneralLayout from "./defaultLayout/Layout";
@@ -23,6 +19,7 @@ import CourseDetailPage from "./pages/CourseDetailPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import InstructorLayout from "./defaultLayout/InstructorLayout";
 import InstructorContent from "./pages/InstructorDashboard/InstructorContent";
+import AdminLayout from "./defaultLayout/AdminLayout";
 import LoadingWrapper from "./components/Loading/LoadingWrapper";
 
 function App() {
@@ -30,6 +27,7 @@ function App() {
     <BrowserRouter>
       <LoadingWrapper>
         <Routes>
+          {/* General Layout */}
           <Route path="/" element={<GeneralLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Loginpage />} />
@@ -51,25 +49,43 @@ function App() {
             </Route>
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
-            {/* Protected routes */}
-            <Route
-              path="/dashboard/admin/*"
-              element={<ProtectedRoute allowedRoles={["admin"]} />}
-            >
-              <Route index element={<AdminPage />} />
-            </Route>
-            <Route
-              path="/dashboard/instructor/*"
-              element={<ProtectedRoute allowedRoles={["instructor"]} />}
-            >
-              <Route index element={<InstructorPage />} />
-            </Route>
             <Route
               path="/dashboard/student/*"
               element={<ProtectedRoute allowedRoles={["student"]} />}
             >
               <Route index element={<StudentPage />} />
             </Route>
+          </Route>
+
+          {/* Admin Layout */}
+          <Route
+            path="/admin"
+            element={<ProtectedRoute allowedRoles={["admin"]} />}
+          >
+            <Route element={<AdminLayout />}>
+              <Route index element={<AdminContent />} />
+              <Route path="dashboard" element={<AdminContent />} />
+              <Route path="users" element={<UserManagement />} />
+              <Route path="request-management" element={<RequestUser />} />
+            </Route>
+          </Route>
+
+          {/* Instructor Layout */}
+          <Route
+            path="/instructor"
+            element={
+              <ProtectedRoute allowedRoles={["instructor"]}></ProtectedRoute>
+            }
+          >
+            <Route element={<InstructorLayout />}>
+              <Route index element={<InstructorContent />} />
+              <Route path="dashboard" element={<InstructorPage />} />
+            </Route>
+          </Route>
+
+          {/* Student Layout */}
+          <Route element={<AdminLayout />}>
+            <Route path="/dashboard/student/*" element={<StudentPage />} />
           </Route>
         </Routes>
       </LoadingWrapper>
