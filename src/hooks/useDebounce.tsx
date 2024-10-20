6dd202a3b21485231d6d1948493161e87fb230ee
Anhsapper <anhntse173567@fpt.edu.vn>
2024-10-20 20:27:32 +0700

    delete text

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,11 +1,5 @@
 import { useEffect, useState } from "react";
 
-// tao debound de search
-// tao query param cho tung truong hop
-// 1: mac dinh la search ""  va selection "all"  -> ?selection="all"&search="null"
-// 2: chi chon search:  search "giatribatkhi"  va selection "all"  -> ?selection="all"&search="giatribatkhi"
-// 3: chi chon selection:  search ""  va selection "new"  -> ?selection="new"&search="null"
-// 4: chon ca 2:  search "giatribatkhi"  va selection "new"  -> ?selection="all"&search="new"
 
 const useDebounce  = (search:string, delay:number) => {
   const [value, setValue] = useState<string>(search);
