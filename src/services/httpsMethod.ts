270a33e792dc7386ed912354f14af083a839eb32
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-10-26 23:07:04 +0700

    update http metho returns

--- src/services/httpsMethod.ts
+++ src/services/httpsMethod.ts
@@ -1,38 +1,29 @@
-import { AxiosError, AxiosResponse } from "axios"; // Import AxiosError từ axios
+import { AxiosResponse } from "axios"; // Import AxiosError từ axios
 import axiosClientVer2 from "./axiosInterceptors";
-import handleError from "./error";
 
 // [GET]
-const getRequest = async <T>(url:string):Promise<AxiosResponse<T, any>> => {
-    const res = await axiosClientVer2.get(url);
-    return res.data
+const getRequest = async <T>(url: string): Promise<AxiosResponse<T, any>> => {
+  const res = await axiosClientVer2.get(url);
+  return res.data;
 };
 
 // [POST]
-const postRequest = async <T>(url:string,payload: any):Promise<AxiosResponse<T, any>> => {
-  const res = await axiosClientVer2.post(url,payload);
-  return res.data
+const postRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
+  const res = await axiosClientVer2.post(url, payload);
+  return res.data;
 };
 
 // [PUT]
-const putRequest = async (url: string, payload: unknown): Promise<unknown> => {
-  try {
-    const res = await axiosClientVer2.put(url, payload);
-    return res.data; // Trả về dữ liệu từ response
-  } catch (error) {
-    return handleError(error as AxiosError); // Chuyển đổi kiểu về AxiosError
-  }
+const putRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
+  const res = await axiosClientVer2.put(url, payload);
+  return res.data;
 };
 
 // [PATCH]
-const patchRequest = async (url: string, payload: unknown): Promise<unknown> => {
-  try {
-    const res = await axiosClientVer2.patch(url, payload);
-    return res.data; // Trả về dữ liệu từ response
-  } catch (error) {
-    return handleError(error as AxiosError); // Chuyển đổi kiểu về AxiosError
-  }
+const patchRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
+  const res = await axiosClientVer2.patch(url, payload);
+  return res.data;
 };
 
-// Xuất các hàm để sử dụng ở nơi khác
+// Export functions for use elsewhere
 export { getRequest, postRequest, putRequest, patchRequest };
