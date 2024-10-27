43e21c855ea716878e4739a9757e1014dbab3593
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-10-27 17:40:32 +0700

    update http method with typing

--- src/services/httpsMethod.ts
+++ src/services/httpsMethod.ts
@@ -1,29 +1,35 @@
-import { AxiosResponse } from "axios"; // Import AxiosError từ axios
+import { AxiosResponse } from "axios"; // Import AxiosResponse from axios
 import axiosClientVer2 from "./axiosInterceptors";
+import { ApiResponse } from "../models/ApiReponse.model";
 
 // [GET]
-const getRequest = async <T>(url: string): Promise<AxiosResponse<T, any>> => {
-  const res = await axiosClientVer2.get(url);
-  return res.data;
+const getRequest = async <T>(url: string): Promise<ApiResponse<T>> => {
+  const res: AxiosResponse<ApiResponse<T>> = await axiosClientVer2.get(url);
+  return res.data; 
 };
 
 // [POST]
-const postRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
-  const res = await axiosClientVer2.post(url, payload);
+const postRequest = async <T>(url: string, payload: unknown): Promise<ApiResponse<T>> => {
+  const res: AxiosResponse<ApiResponse<T>> = await axiosClientVer2.post(url, payload);
   return res.data;
 };
 
 // [PUT]
-const putRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
-  const res = await axiosClientVer2.put(url, payload);
+const putRequest = async <T>(url: string, payload: unknown): Promise<ApiResponse<T>> => {
+  const res: AxiosResponse<ApiResponse<T>> = await axiosClientVer2.put(url, payload);
   return res.data;
 };
 
 // [PATCH]
-const patchRequest = async <T>(url: string, payload: unknown): Promise<AxiosResponse<T, any>> => {
-  const res = await axiosClientVer2.patch(url, payload);
-  return res.data;
+const patchRequest = async <T>(url: string, payload: unknown): Promise<ApiResponse<T>> => {
+  const res: AxiosResponse<ApiResponse<T>> = await axiosClientVer2.patch(url, payload);
+  return res.data; 
+};
+
+const deleteRequest = async <T>(url: string): Promise<ApiResponse<T>> => {
+  const res: AxiosResponse<ApiResponse<T>> = await axiosClientVer2.delete(url);
+  return res.data; // Ensure the returned data matches ApiResponse<T>
 };
 
 // Export functions for use elsewhere
-export { getRequest, postRequest, putRequest, patchRequest };
+export { getRequest, postRequest, putRequest, patchRequest, deleteRequest };
