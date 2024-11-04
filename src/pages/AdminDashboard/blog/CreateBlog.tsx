11869ff17dea8615baf5d236866ba42594072efb
VinhHT2910 <vinhcute11023@gmail.com>
2024-11-04 15:18:29 +0700

    add image in create blog & user profile

--- src/pages/AdminDashboard/blog/CreateBlog.tsx
+++ src/pages/AdminDashboard/blog/CreateBlog.tsx
@@ -1,56 +1,38 @@
-
-import { Button, Form, Upload, Input, Select, Image, message } from "antd";
+import { Button, Form, Upload, Input, Select, message } from "antd";
 import React, { useEffect, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import type { UploadFile, UploadProps } from "antd";
-import "./CreateBlog.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import CategoryService from "../../../services/category.service";
-import { Category, GetCategories } from "../../../models/Category.model";
+import { Category } from "../../../models/Category.model";
 import { BlogRequest } from "../../../models/Blog.model";
 import BlogService from "../../../services/blog.service";
+import { API_UPLOAD_FILE } from "../../../constants/upload";
 const { Option } = Select;
 
 type BlogFormProps = {
   initialValues?: BlogRequest;
-  onSuccess?: () => void; // Add a success callback prop
+  onSuccess?: () => void;
 };
 
-const CreateBlog: React.FC<BlogFormProps> = ({
-  initialValues,
-  onSuccess,
-}) => {
-  const [previewImage, setPreviewImage] = useState<string | null>(null);
-  const [previewOpen, setPreviewOpen] = useState<boolean>(false);
+const CreateBlog: React.FC<BlogFormProps> = ({ initialValues, onSuccess }) => {
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const [listCategories, setListCategories] = useState<Category[]>([]);
   const [form] = Form.useForm<BlogRequest>();
 
-  const initialCategoriesParams: GetCategories = {
-    pageInfo: {
-      pageNum: 1,
-      pageSize: 100,
-    },
-    searchCondition: {
-      keyword: "",
-      is_deleted: false,
-    },
-  };
-
   const fetchCategories = async () => {
-    const categoriesResponse = await CategoryService.getCategories(initialCategoriesParams);
+    const categoriesResponse = await CategoryService.getCategories({
+      pageInfo: { pageNum: 1, pageSize: 100 },
+      searchCondition: { keyword: "", is_deleted: false },
+    });
     setListCategories(categoriesResponse.data?.pageData ?? []);
   };
 
   useEffect(() => {
     fetchCategories();
     if (initialValues) {
-      form.setFieldsValue({
-        ...initialValues,
-        content: initialValues.content,
-      });
-      setPreviewImage(initialValues.image_url);
+      form.setFieldsValue({ ...initialValues, content: initialValues.content });
       setFileList([
         {
           uid: "-1",
@@ -62,58 +44,36 @@ const CreateBlog: React.FC<BlogFormProps> = ({
     }
   }, [initialValues, form]);
 
-  const handlePreview = async (file: UploadFile) => {
-    setPreviewImage(file.url || (file.preview as string));
-    setPreviewOpen(true);
-  };
-
-  const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) => {
+  const handleImageChange: UploadProps["onChange"] = ({ fileList: newFileList }) => {
     setFileList(newFileList);
-    if (newFileList.length > 0) {
-      const file = newFileList[0];
-      setPreviewImage(file.url || (file.preview as string));
+
+    if (newFileList.length > 0 && newFileList[0].status === "done") {
+      const uploadedImageUrl = newFileList[0].response?.secure_url;
+      form.setFieldsValue({ image_url: uploadedImageUrl });
+      console.log("image url:", uploadedImageUrl)
+    } else if (newFileList.length === 0 || newFileList[0].status === "error") {
+      form.setFieldsValue({ image_url: "" });
     }
   };
-
-  const uploadButton = (
-    <button style={{ border: 0, background: "none" }} type="button">
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </button>
-  );
 
   const onFinish = async (values: BlogRequest) => {
-    values.image_url = "https://picsum.photos/id/237/200/300";
     try {
-      const response = await BlogService.createBlog(values); // Create the blog using BlogService
-      if (response && response.success && onSuccess) {
+      const response = await BlogService.createBlog(values);
+      if (response?.success && onSuccess) {
         message.success("Blog created successfully");
         onSuccess();
-        form.resetFields(); // Reset form fields
-        setFileList([]); // Clear uploaded files
-        setPreviewImage(null); // Clear preview image
-        return response.data; // Trả về dữ liệu người dùng đã được cập nhật 
+        form.resetFields();
+        setFileList([]);
       } else {
-        message.error("Failed to create blog"); // Hiển thị thông báo lỗi
-        return null; // Trả về null khi không thành công
+        message.error("Failed to create blog");
       }
-
     } catch (error) {
-      console.error("Error create blog:", error);
-      message.error("An error occurred while creating the blog."); // Thông báo lỗi chung
-      return null; // Đảm bảo trả về null trong trường hợp có lỗi
+      message.error("An error occurred while creating the blog.");
     }
   };
 
-
   return (
-    <Form
-      form={form}
-      name="create-blog"
-      onFinish={onFinish}
-      layout="vertical"
-      style={{ display: "flex", height: "100%" }}
-    >
+    <Form form={form} name="create-blog" onFinish={onFinish} layout="vertical" style={{ display: "flex", height: "100%" }}>
       {/* Left Section */}
       <div style={{ flex: 1, paddingRight: "16px", overflowY: "auto" }}>
         <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please enter the blog name!" }]}>
@@ -131,32 +91,24 @@ const CreateBlog: React.FC<BlogFormProps> = ({
         </Form.Item>
 
         <Form.Item label="Description" name="description" rules={[{ required: true, message: "Please enter the blog description!" }]}>
-          <Input.TextArea placeholder="Enter blog description" maxLength={500} showCount allowClear rows={4} />
+          <Input.TextArea placeholder="Enter blog description" maxLength={1000} rows={2} />
         </Form.Item>
 
         <Form.Item label="Title Image" name="image_url">
           <Upload
-            action="https://your-api-server.com/admin/upload"
+            action={API_UPLOAD_FILE}
             listType="picture-card"
-            onPreview={handlePreview}
-            onChange={handleChange}
+            onChange={handleImageChange}
             fileList={fileList}
             maxCount={1}
           >
-            {fileList.length > 0 ? null : uploadButton}
+            {fileList.length < 1 && (
+              <button style={{ border: 0, background: "none" }} type="button">
+                <PlusOutlined />
+                <div>Upload</div>
+              </button>
+            )}
           </Upload>
-
-          {previewImage && (
-            <Image
-              wrapperStyle={{ display: "none" }}
-              preview={{
-                visible: previewOpen,
-                onVisibleChange: (visible) => setPreviewOpen(visible),
-                afterOpenChange: (visible) => !visible && setPreviewImage(""),
-              }}
-              src={previewImage}
-            />
-          )}
         </Form.Item>
 
         <Form.Item>
@@ -166,8 +118,8 @@ const CreateBlog: React.FC<BlogFormProps> = ({
         </Form.Item>
       </div>
 
-      {/* Right Section - CKEditor */}
-      <div style={{ flex: 1, paddingLeft: "16px", overflowY: "auto" }}>
+      {/* CKEditor Section */}
+      <div style={{ flex: 1, paddingLeft: "16px" }}>
         <Form.Item label="Content" name="content" rules={[{ required: true, message: "Please enter the blog content!" }]}>
           <CKEditor
             editor={ClassicEditor}
--- src/pages/BlogDetailPage.tsx
+++ src/pages/BlogDetailPage.tsx
@@ -8,8 +8,9 @@ import { Blog } from "../models/Blog.model";
 import { useEffect, useState } from "react";
 import BlogDetailSkeleton from "../components/Blogs/BlogDetailSkeleton";
 import ClientService from "../services/client.service";
+import parse from "html-react-parser";
 
-
+// Sample comments data
 const comments = [
   {
     avatar: "https://picsum.photos/seed/picsum/200/300",
@@ -17,65 +18,13 @@ const comments = [
     date: "2024-07-18T04:16:27.274Z",
     content: "Great blog",
   },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
-  {
-    avatar: "https://picsum.photos/seed/picsum/200/300",
-    name: "John Doe",
-    date: "2024-07-18T04:16:27.274Z",
-    content: "Great blog",
-  },
+  // ... other comments
 ];
-
-const HTMLContent = ({ content }: { content: string }) => {
-  return (
-    <div 
-      className="prose max-w-none" // if you're using Tailwind Typography
-      dangerouslySetInnerHTML={{ __html: content }} 
-    />
-  );
-};
 
 const BlogDetailPage = () => {
   const { id } = useParams();
   const [blog, setBlog] = useState<Blog>();
   const [isLoading, setIsLoading] = useState(true);
-
-  
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -83,17 +32,18 @@ const BlogDetailPage = () => {
         try {
           setIsLoading(true);
           const response = await ClientService.getBlogDetail(id);
+          console.log("data", response);
           if (response.data) {
-            setBlog(response.data);
+            setBlog(response.data); 
           }
         } catch (error) {
-          console.error('Error fetching blog:', error);
+          console.error("Error fetching blog:", error);
         } finally {
           setIsLoading(false);
         }
       }
     };
-    
+
     fetchBlogs();
   }, [id]);
 
@@ -105,21 +55,38 @@ const BlogDetailPage = () => {
     return <div>Blog not found</div>;
   }
 
+  const renderContent = (content: string) => {
+    return parse(content, {
+      replace: (domNode: any) => {
+        if (domNode.name === "oembed" && domNode.attribs?.url) {
+          const url = domNode.attribs.url;
+          return (
+            <iframe
+              width="100%"
+              height="400"
+              src={url.replace("youtu.be", "youtube.com/embed")}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          );
+        }
+      },
+    });
+  };
+
   return (
     <div className="font-exo flex mt-6">
       <div className="w-3/4">
         <BlogHeader
           title={blog.name}
           date={new Date(blog.created_at).toLocaleDateString()}
-          creator={blog.user_id}
-          comments={0}
+          creator={blog.user_name}
+          comments={comments.length}
         />
-        <img
-          src={blog.image_url}
-          alt="blog"
-          className="w-full h-[400px] mt-8 rounded-2xl object-contain"
-        />
-        <div className="mt-8"><HTMLContent content={blog.content}/></div>
+        <div className="w-full">
+          {renderContent(blog.content)}
+        </div>
         <TagList tags={["1", "2", "6", "7"]} />
         <div>
           <h2 className="text-lg font-semibold mt-8">Recent Blogs</h2>
