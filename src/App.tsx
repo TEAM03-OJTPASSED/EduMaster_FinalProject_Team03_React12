01778c3b59b4f2f8ce0414bfb3944a453cba53a7
datquang03 <daoquangdat0103@gmail.com>
2024-11-04 10:17:17 +0700

    fix app.tsx temporary

--- src/App.tsx
+++ src/App.tsx
@@ -36,9 +36,9 @@ import InstructorReview from "./pages/InstructorDashboard/InstructorReview";
 import InstructorSetting from "./pages/InstructorDashboard/instructor-setting/InstructorSetting";
 import ForgotPasswordPage from "./pages/AuthPage/ForgotPasswordPage";
 import PayoutManagement from "./pages/AdminDashboard/payoutManagement";
-import RequestPayout from "./pages/InstructorDashboard/instructor-management/payout/RequestPayout";
+// import RequestPayout from "./pages/InstructorDashboard/instructor-management/payout/RequestPayout";
 import CompletedPayout from "./pages/InstructorDashboard/instructor-management/payout/CompletedPayout";
-import RejectedPayout from "./pages/InstructorDashboard/instructor-management/payout/RejectedPayout";
+// import RejectedPayout from "./pages/InstructorDashboard/instructor-management/payout/RejectedPayout";
 import DashboardLayout from "./defaultLayout/DashboardLayout";
 import StudentProfile from "./pages/StudentDashboard/studentProfile";
 import StudentCourses from "./pages/StudentDashboard/StudentCourses";
@@ -60,7 +60,7 @@ import InstructorProfile from "./pages/InstructorDashboard/instructor-setting/In
 import AdminChangePassword from "./pages/AdminDashboard/setting/AdminChangePassword";
 import AdminSetting from "./pages/AdminDashboard/setting/AdminSetting";
 import AdminProfile from "./pages/AdminDashboard/setting/AdminProfile";
-import InstructorSalesHistory from "./pages/InstructorDashboard/instructor-management/InstructorOrdersHistory";
+// import InstructorSalesHistory from "./pages/InstructorDashboard/instructor-management/InstructorOrdersHistory";
 import LearnCoursePage from "./pages/LearnCoursePage";
 import TopUpPage from "./pages/topup/TopupPage";
 import AdminRequestPayout from "./pages/AdminDashboard/payout/RequestPayout";
@@ -77,11 +77,12 @@ function App() {
   useEffect(() => {
     const init = () => {
       gapi.client.init({
-        clientId:"67368420889-utrdru1873d1pudjah97ihj32vvfire8.apps.googleusercontent.com",
-        scope:""
+        clientId:
+          "67368420889-utrdru1873d1pudjah97ihj32vvfire8.apps.googleusercontent.com",
+        scope: "",
       });
     };
-    gapi.load("client:auth2",init)
+    gapi.load("client:auth2", init);
   }, []);
   return (
     <BrowserRouter>
@@ -126,8 +127,6 @@ function App() {
               <Route path="/firebase" element={<Firebase />} />
 
               <Route path="/learn/:id" element={<LearnCoursePage />} />
-
-              
             </Route>
 
             {/* Admin Layout */}
@@ -192,16 +191,16 @@ function App() {
                 <Route path="orders" element={<StudentOrderHistory />} />
 
                 <Route path="payout" element={<InstructorPayout />}>
-                  <Route index element={<RequestPayout />} />
+                  {/* <Route index element={<RequestPayout />} /> */}
                   <Route
                     path="completed-payout"
                     element={<CompletedPayout />}
                   />
-                  <Route path="rejected-payout" element={<RejectedPayout />} />
+                  {/* <Route path="rejected-payout" element={<RejectedPayout />} /> */}
                 </Route>
                 <Route
                   path="salesHistory"
-                  element={<InstructorSalesHistory />}
+                  // element={<InstructorSalesHistory />}
                 />
                 <Route path="my-learning" element={<InstructorLearning />} />
                 <Route path="my-courses" element={<InstructorCourses />}>
