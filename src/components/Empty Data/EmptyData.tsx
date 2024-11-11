6d8c8172b145cd1cbf1ed49b02132928aaaa9c13
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-11-11 09:00:56 +0700

    add empty data description component

---
+++ src/components/Empty Data/EmptyData.tsx
@@ -0,0 +1,20 @@
+import { InboxOutlined } from "@ant-design/icons";
+import { Empty } from "antd";
+
+interface EmptyDataProps {
+  message: string;
+  description: string;
+}
+
+const EmptyData = ({ message, description }: EmptyDataProps) => (
+  <Empty
+    description={
+      <div>
+        <p className="text-gray-600 mb-2">{message}</p>
+        <p className="text-gray-400 text-sm">{description}</p>
+      </div>
+    }
+  />
+);
+
+export default EmptyData;
