a9ff1663d45f2acefc557fa5e73b0c46367fa3ea
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-11-29 18:50:33 +0700

    scroll to top automatically on nav

--- src/hooks/customNavigate.tsx
+++ src/hooks/customNavigate.tsx
@@ -3,20 +3,21 @@ import { useNavigate } from "react-router-dom";
 export const useCustomNavigate = () => {
   const navigate = useNavigate();
 
-  const navigateTo = (path: string, newTab?: boolean, e?: React.MouseEvent<HTMLAnchorElement>) => {
-    
+  const navigateTo = (
+    path: string,
+    newTab?: boolean,
+    e?: React.MouseEvent<HTMLAnchorElement>
+  ) => {
+    e?.preventDefault();
+    e?.stopPropagation();
+
     if (newTab) {
-      e?.preventDefault()
-      e?.stopPropagation()
-      window.open(path, "_blank"); 
+      window.open(path, "_blank");
     } else {
-      e?.preventDefault()
-      e?.stopPropagation()
-      navigate(path); 
+      window.scrollTo({ top:0, behavior: "instant"})
+      navigate(path);
     }
   };
 
   return navigateTo;
 };
-
-
